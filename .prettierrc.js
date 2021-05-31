50b06a4f444314048bc41d69153156869605567f
Felix Mutua <felixmutua01@gmail.com>
2021-05-31 14:04:47 +0300

    Firebase Auth

---
+++ .prettierrc.js
@@ -0,0 +1,4 @@
+export const allowParens = 'always';
+export const singleQuote = true;
+export const tabWidth = 2;
+export const trailingComa = 'none';
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
+import { useAuth } from '../lib/auth'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
+ const Home = () => {
+  const auth = useAuth();
+  return( <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Fast Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -15,39 +16,16 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Current User : <code>{auth.user? auth.user.email : null}</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <button onClick = {(e)=>auth.signInWithGithub()}>Sign In</button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <div>{auth?.user?.email}</div>
+        {auth?.user && (
+          <button onClick={(e)=>auth.signout()}>Sign out</button>
+        )}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
@@ -61,5 +39,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+    );
 }
+export default Home;
